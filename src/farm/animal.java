deeebe47a364028bcef9f3320eebcb2ba3e6dddb
danny2708 <duyanh27082005@gmail.com>
2024-10-07 16:32:55 +0700

    first commit

---
+++ src/farm/animal.java
@@ -0,0 +1,24 @@
+public class Lon {
+    private boolean can_Swim;
+    private String name;
+    public animal (boolean can_Swim,String name) {
+        this.can_Swim = can_Swim;
+        this.name = name;
+    }
+
+    public boolean isCan_Swim() {
+        return can_Swim;
+    }
+
+    public void setCan_Swim(boolean can_Swim) {
+        this.can_Swim = can_Swim;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/farm/pig.java
@@ -0,0 +1,5 @@
+public class pig extends animal {
+    public pig (boolean can_Swim, String name) {
+        super(can_Swim,name);
+    }
+}
